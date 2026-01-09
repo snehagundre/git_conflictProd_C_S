f334e0d9906da8d1be9f48853cccf67045ea72e3
Nayna-patil-2002 <nayanashinde2002@gmail.com>
2026-01-09 23:19:00 +0530

    cardChanges

--- src/app/shared/components/prod-form/prod-form.component.ts
+++ src/app/shared/components/prod-form/prod-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import { error } from 'console';
+import { IProduct } from '../../models/prod';
 
 @Component({
   selector: 'app-prod-form',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prod-form.component.scss']
 })
 export class ProdFormComponent implements OnInit {
-
-  constructor() { }
+   productForm!:FormGroup
+   isIneditMode:boolean=false
+   editObj!:IProduct
+  constructor(
+    private _productService:ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.createProducTform()
+    this.editprod()
+  }
+
+  createProducTform(){
+    this.productForm=new FormGroup({
+      name:new FormControl(null, [Validators.required]),
+      description:new FormControl(null, [Validators.required])
+    })
+  }
+
+  productAdd(){
+    
+  }
+
+  editprod(){
+  this._productService.editProduct$
+    .subscribe({
+      next:res=>{
+        console.log(res)
+        this.editObj=res
+        this.productForm.patchValue(res)
+        this.isIneditMode=true
+
+      }
+     
+    })
+  }
+
+  onUpdate(){
+    if(this.productForm.valid){
+      let updateObj={...this.productForm.value, id:this.editObj.id}
+      console.log(updateObj)
+      this._productService.updateProduct(updateObj)
+    }
   }
 
 }
